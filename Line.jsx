453c6cf9b2c84e831c6c10f1893504b9e974dc5c
LOKESHBADELA <146653674+LOKESHBADELA@users.noreply.github.com>
2024-09-09 21:58:30 +0530

    Add files via upload

---
+++ Line.jsx
@@ -0,0 +1,10 @@
+// eslint-disable-next-line no-unused-vars
+import React from 'react'
+
+function Line() {
+  return (
+      <div className="mt-5 pt-5 border-b-2 border-l-blue-900"></div>
+  )
+}
+
+export default Line
