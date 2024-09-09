7c571c7f8438b627bb1b098e11fb0c1f07016a9e
LOKESHBADELA <146653674+LOKESHBADELA@users.noreply.github.com>
2024-09-09 21:55:37 +0530

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,20 @@
+import Body from "./components/Body"
+import Head from "./components/Head"
+// eslint-disable-next-line no-unused-vars
+import React from "react"
+
+
+function App() {
+  return (
+    <>
+        <div>
+          <Head/>
+          <Body/>
+        </div>
+
+    </>
+  )
+}
+
+export default App
+
